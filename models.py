6e9ef68f3a830f1a9b1b24c48286d610ae4113d2
Artem Fomin <ezidze09@mail.ru>
2019-01-29 12:02:40 +0300

    Добавлен файл models.py

---
+++ models.py
@@ -0,0 +1,32 @@
+import datetime
+
+class Book():
+    def __init__(self, title, author, date):
+        self.title = title
+        self.author = author
+        self.datePublished = date
+        self.borrowed = None
+        self.dateBorrow = None
+        self.dateReturn = None
+
+    def borrow(self, borrowed):
+        self.borrowed = borrowed
+        self.dateBorrow = datetime.date.today()
+        self.dateBorrow = datetime.date.today() \
+                            + datetime.timedelta(weeks=2)
+
+    def __str__(self):
+        return '{}\t{}\t{}'.format(self.title, self.author, self.datePublished)
+
+
+class People():
+    def __init__(self, name, date):
+        self.name = name
+        self.dateBirth = date
+        self.books = list()
+
+    def addBook(self, book):
+        self.books.append(book)
+
+    def delBook(self):
+        self.books.pop()
